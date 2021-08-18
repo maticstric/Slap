11f74181761675042a64d5d3e162a1cc7891f160
maticstric <matic@likar.me>
2021-08-18 16:55:32 -0700

    Fix level02 platform jitter

---
+++ Assets/Project/Scripts/Level/Level02/PlatformMover02.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Mirror;
+
+public class PlatformMover02 : NetworkBehaviour {
+    [Header("Stats")]
+    [SerializeField] private float movementDuration;
+    [SerializeField] private float finalPosition;
+
+    [Header("Curves")]
+    [SerializeField] private AnimationCurve animationCurve;
+
+    private Rigidbody _rigidbody;
+    private float _initialPosition;
+    private float _time;
+
+    private void Awake() {
+        _rigidbody = GetComponent<Rigidbody>();
+        _initialPosition = transform.position.y;
+        _time = 0;
+    }
+
+    private void FixedUpdate() {
+        MovePlatform();
+    }
+
+    private void MovePlatform() {
+        float percent = (_time % movementDuration) / movementDuration;
+        float position = Mathf.Lerp(_initialPosition, finalPosition, animationCurve.Evaluate(percent));
+
+        _rigidbody.MovePosition(new Vector3(transform.position.x, position, transform.position.z));
+
+        _time += Time.fixedDeltaTime;
+    }
+}
